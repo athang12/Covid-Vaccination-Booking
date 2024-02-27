a14afb1fd1f117b04710a12aa04fbeb7c0455a29
athang12 <athangyende459@gmail.com>
2024-02-28 01:06:05 +0530

    sup

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import find_packages, setup
+
+requirements = ['flask','Flask','Gunicorn','flask_login','flask_mysqldb','flask_bcrypt','datetime','dotenv','setuptools>=57.0.0','wheel>=0.36.2'] # Add your required packages her
